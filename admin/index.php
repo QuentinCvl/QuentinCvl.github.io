49cc2784d10869a1e6553600c8d66a006877d90b
Quentin Cuvelier <quentincuvelier@laposte.net>
2021-10-09 12:46:33 +0200

    login Form

--- admin/index.php
+++ admin/index.php
@@ -1,18 +1,6 @@
 <?php
-require_once("vendor/autoload.php");
+require_once("../vendor/autoload.php");
 
-require_once('controller/controller.php');
+require_once('login.php');
 
-if (isset ($_GET['page'])) {
-  if($_GET['page'] === "home") {
-    listPosts();
-  } elseif($_GET['page'] === "contact") {
-    contact();
-  }
-} elseif (isset ($_GET['page']) && file_exists($_GET['page'] . '.html')) {
-  require_once($_GET['page'] . '.html');
-} elseif (!isset($_GET['page'])) {
-  listPosts();
-} else {
-  require_once("404.html");
-}
+
--- admin/login.php
+++ admin/login.php
@@ -1,1 +1,48 @@
-<?php
+<!DOCTYPE html>
+<html class="no-js" lang="fr">
+<head>
+  <meta charset="utf-8">
+  <title>Electicism. - Connexion</title>
+  <meta name="description" content="">
+  <meta name="author" content="">
+  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+
+  <link rel="stylesheet" href="../public/css/base.css">
+  <link rel="stylesheet" href="../public/css/vendor.css">
+  <link rel="stylesheet" href="../public/css/main.css">
+
+  <script src="../public/js/modernizr.js"></script>
+  <script src="../public/js/pace.min.js"></script>
+
+  <link rel="shortcut icon" href="../favicon.ico" type="image/x-icon">
+  <link rel="icon" href="../favicon.ico" type="image/x-icon">
+</head>
+<body id="top">
+<section class="s-pageheader full-height">
+  <header class="header">
+    <div class="header__content row">
+      <div class="header__logo">
+        <a class="logo" href="">
+          <img src="../public/images/Eclecticism-nobg.png" alt="Homepage">
+        </a>
+      </div>
+    </div>
+  </header>
+  <div style="width: 100%; height: 100%; display: flex; justify-content: center; position: relative;">
+    <form name="loginForm" id="loginForm" method="post" action="">
+      <fieldset>
+        <legend>Connexion Administrateur</legend>
+
+        <div class="form-field">
+          <input type="text" class="full-width" id="loginID" name="loginID" placeholder="Identifiant">
+        </div>
+
+        <div class="form-field">
+          <input type="text" class="full-width" id="loginPSWD" name="loginPSWD" placeholder="Mot de passe">
+        </div>
+
+        <button type="submit" class="submit btn btn--primary full-width">Connexion</button>
+      </fieldset>
+    </form>
+  </div>
+</section>
